ee97a7d82e398cf1f4c1ee640d0e4d17ef1c7e02
Lographa <81043894+Lographa@users.noreply.github.com>
2022-06-07 12:58:39 -0300

    backcommit

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,9 +4,8 @@ const { async } = require('regenerator-runtime');
 
 
 exports.index = (req, res) => {
-  res.render('login');
-  // if(req.session.user) return res.render('login-logado');
-  // return res.render('login');
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
 };
 
 exports.register = async function (req, res) {
@@ -21,7 +20,7 @@ exports.register = async function (req, res) {
       });
       return;
     }
-    req.flash("success", "logado com sucesso")
+    req.flash("success", "Criado com sucesso")
     req.session.save(function () {
       return res.redirect('/login/index');
     })
@@ -30,58 +29,34 @@ exports.register = async function (req, res) {
     console.log(error)
     return res.render('404')
   }
- 
+};
+
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+  
+    if(login.errors.length > 0){
+      req.flash("errors", login.errors)
+      req.session.save(function () {
+        return res.redirect('/login/index');    
+      });
+      return;
+    };
+
+    req.flash("success", "logado com sucesso");
+    req.session.user = login.user;
+    req.session.save(function () {
+      return res.redirect('/login/index');
+    })
+    // return res.send(login.errors);
+  } catch (error) {
+    console.log(error)
+    return res.render('404')
+  }
+};
+
+exports.logout = function (req, res) {
+  req.session.destroy();
+  res.redirect('/');
 }
-
-// exports.register = async function(req, res) {
-//   try {
-//     const login = new Login(req.body);
-//     await login.register();
-
-//     if(login.errors.length > 0) {
-//       req.flash('errors', login.errors);
-//       req.session.save(function() {
-//         return res.redirect('back');
-//       });
-//       return;
-//     }
-
-//     req.flash('success', 'Seu usuário foi criado com sucesso.');
-//     req.session.save(function() {
-//       return res.redirect('back');
-//     });
-//   } catch(e) {
-//     console.log(e);
-//     return res.render('404');
-//   }
-// };
-
-// exports.login = async function(req, res) {
-//   try {
-//     const login = new Login(req.body);
-//     await login.login();
-
-//     if(login.errors.length > 0) {
-//       req.flash('errors', login.errors);
-//       req.session.save(function() {
-//         return res.redirect('back');
-//       });
-//       return;
-//     }
-
-//     req.flash('success', 'Você entrou no sistema.');
-//     req.session.user = login.user;
-//     req.session.save(function() {
-//       return res.redirect('back');
-//     });
-//   } catch(e) {
-//     console.log(e);
-//     return res.render('404');
-//   }
-// };
-
-// exports.logout = function(req, res) {
-//   req.session.destroy();
-//   res.redirect('/');
-// };
-
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -16,6 +16,27 @@ class Login {
     this.user = null;
   }
 
+  async login() {
+    this.valida();
+    
+    if (this.errors.length > 0) return;
+    //procura email
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    //verifica se existe
+    if(!this.user){
+      this.errors.push("Usuário não existe");
+      return;
+    };
+
+    //verifica a senha encriptada
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)){
+      this.errors.push("Senha inválida");
+      this.user= null;
+      return;
+    };
+  };
+
   async register() {
     this.valida();
     //verifica se tem erros
@@ -28,18 +49,14 @@ class Login {
     const salt = bcryptjs.genSaltSync();
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-    try {
-      this.user = await LoginModel.create(this.body);
-    } catch (error) {
-      console.log(error)
-    }
-
-  }
+   
+    this.user = await LoginModel.create(this.body);
+  };
 
   //verifica se o email ja existe
   async userExists() {
-    const user = await LoginModel.findOne({ email: this.body.email });
-    if(user) this.errors.push("Usuário já cadastrado com esse e-mail");
+    this.user = await LoginModel.findOne({ email: this.body.email });
+    if(this.user) this.errors.push("Usuário já cadastrado com esse e-mail");
   }
 
   valida() {
@@ -64,71 +81,6 @@ class Login {
       email: this.body.email,
       password: this.body.password
     };
-  }
-
-}
-
-//   async login() {
-//     this.valida();
-//     if(this.errors.length > 0) return;
-//     this.user = await LoginModel.findOne({ email: this.body.email });
-
-//     if(!this.user) {
-//       this.errors.push('Usuário não existe.');
-//       return;
-//     }
-
-//     if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
-//       this.errors.push('Senha inválida');
-//       this.user = null;
-//       return;
-//     }
-//   }
-
-//   async register() {
-//     this.valida();
-//     if(this.errors.length > 0) return;
-
-//     await this.userExists();
-
-//     if(this.errors.length > 0) return;
-
-//     const salt = bcryptjs.genSaltSync();
-//     this.body.password = bcryptjs.hashSync(this.body.password, salt);
-
-//     this.user = await LoginModel.create(this.body);
-//   }
-
-//   async userExists() {
-//     this.user = await LoginModel.findOne({ email: this.body.email });
-//     if(this.user) this.errors.push('Usuário já existe.');
-//   }
-
-//   valida() {
-//     this.cleanUp();
-
-//     // Validação
-//     // O e-mail precisa ser válido
-//     if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
-
-//     // A senha precisa ter entre 3 e 50
-//     if(this.body.password.length < 3 || this.body.password.length > 50) {
-//       this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
-//     }
-//   }
-
-//   cleanUp() {
-//     for(const key in this.body) {
-//       if(typeof this.body[key] !== 'string') {
-//         this.body[key] = '';
-//       }
-//     }
-
-// this.body = {
-//   email: this.body.email,
-//   password: this.body.password
-// };
-//   }
-// }
-
+  };
+};
 module.exports = Login;
